975c019ab44e30052bb8615c65243d20a1805a2f
Jason Winning <jason.winning@gmail.com>
2025-10-09 19:36:39 -0500

    Fix SourceForge ticket #85: Duplicate folder ID error; version 1.32.1 release
    Signed-off-by: Jason Winning <jason.winning@gmail.com>

--- src/main/java/org/hypernomicon/Const.java
+++ src/main/java/org/hypernomicon/Const.java
@@ -41,7 +41,7 @@ public final class Const
    * Change this and the version in pom.xml to set the application version
    * <br>
    */
-  public static final VersionNumber appVersion = new VersionNumber(1, 32);  // 1.32
+  public static final VersionNumber appVersion = new VersionNumber(1, 32, 1);  // 1.32.1
 
   /**
    * This is the minimum version that the application version (or higher) is able to load
